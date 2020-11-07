2d922a03911b0969da71182bbbadc457806563fd
Eben60 <8xe8n-cm@online.de>
2020-11-08 00:03:14 +0100

    clean up a bit

--- src/Julia/ZMQ_utils.jl
+++ src/Julia/ZMQ_utils.jl
@@ -57,35 +57,6 @@ function puttogether(;
    return r
 end
 
-
-function puttogether_old(;
-                      bin_data::Bytar=UInt8[],
-                      jsonstring="{\"status\":false,\"source\":\"\",\"code\":0,\"longdescr\":\"this is a looong description\"}",
-                      err=err_dict(),
-                      opt_header::Bytar=UInt8[],
-                      shorterrcode::Int=0
-                      )
-
-   err = Bytar(JSON3.write(err))
-   if jsonstring != ""
-      JSON3.read(jsonstring) # just to check validity; TODO - add error processing code
-   end
-
-   jsonstring = Bytar(jsonstring)
-
-   shorterrcode = UInt8(shorterrcode)
-
-   o_h_lng = int2bytar(length(opt_header))
-   err_lng = int2bytar(length(err))
-   bin_lng = int2bytar(length(bin_data))
-   js_lng = int2bytar(length(jsonstring))
-
-   r = vcat(shorterrcode, PROTOC_V, o_h_lng, err_lng, bin_lng, opt_header, err, bin_data, jsonstring)
-
-
-   return r
-end
-
 function parse_cmnd(b)
    c = b[1]
    prot_v = b[2]
