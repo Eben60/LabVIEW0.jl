d9dd397e040f75e179674183ae9b72ebdd93b0f3
Ben Elkin <8xe8n-cm@online.de>
2020-11-07 10:45:23 +0100

    Julia images

---
+++ src/Julia/this-and-that/colors_and_images.jl
@@ -0,0 +1,17 @@
+using Colors, TestImages, QuartzImageIO, ImageShow, FileIO
+
+# see https://juliaimages.org
+
+peppers = FileIO.load("src/Julia/this-and-that/peppers_color_256.png")
+
+img = testimage("lena_color_256")
+#img = testimage("lena_color_512")
+
+display(img)
+
+p55=img[5,5]
+println(p55)
+println(typeof(p55))
+println(p55.r, p55.g, p55.b)
+
+p55_g = reinterpret(UInt8, p55.g) # 0x85 = 133
