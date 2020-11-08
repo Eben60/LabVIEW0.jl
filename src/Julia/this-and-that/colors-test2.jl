e4ec731a6796900c83053f44f538acc66bbf57f5
Eben60 <8xe8n-cm@online.de>
2020-11-08 23:45:02 +0100

    small test file

---
+++ src/Julia/this-and-that/colors-test2.jl
@@ -0,0 +1,12 @@
+using TestImages, ImageCore
+
+img = testimage("lena_color_256")
+
+cv = channelview(img)
+r=collect(reinterpret(UInt8, cv))
+@show typeof(r), size(r) # this is what I have
+
+rc = r/255.0
+
+im2 = colorview(RGB, rc)
+display(im2)
