7a865d4fe79ab4c82e317522e72ab8e6fef4ec06
Eben60 <8xe8n-cm@online.de>
2020-12-28 14:56:47 +0100

    this-and-that

--- src/Julia/this-and-that/tmp-kwargs.jl
+++ src/Julia/this-and-that/tmp-kwargs.jl
@@ -1,10 +1,13 @@
 function foo(;kwargs...)
     @show kwargs[:a]
     @show keys(kwargs)
-    if ! (:c in keys(kwargs))
-        c=3
-    else
-        c=kwargs[:c]
-    end
+    # if ! (:c in keys(kwargs))
+    #     c=3
+    # else
+    #     c=kwargs[:c]
+    # end
+    c = get(kwargs, :c, 3)
+
     @show c
+    return nothing
 end
