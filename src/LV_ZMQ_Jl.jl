efc5feb79ce70230e0d0e095918698d346ccc045
Eben60 <8xe8n-cm@online.de>
2021-01-02 17:10:37 +0100

    version update

--- src/LV_ZMQ_Jl.jl
+++ src/LV_ZMQ_Jl.jl
@@ -8,7 +8,7 @@ scriptexists = false
 scriptOK = true
 scriptexcep = nothing
 
-export ZMQ_server, get_script_path # functions
+export ZMQ_server, get_script_path, get_LVlib_path # functions
 export scriptexists, scriptOK, scriptexcep # global variables
 
 end
