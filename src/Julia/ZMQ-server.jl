830542192c8488d4eb24376f7a342101e052a27d
Eben60 <8xe8n-cm@online.de>
2021-01-23 22:32:11 +0100

    added optional kwarg initOK to server_0mq4lv()

--- src/Julia/ZMQ-server.jl
+++ src/Julia/ZMQ-server.jl
@@ -3,24 +3,24 @@ include("./test_functions.jl")
 # include("./user_functions.jl")
 
 """
-    server_0mq4lv(fns=(;))
+    server_0mq4lv(fns=(;); initOK=false)
 
 `server_0mq4lv` (meaning zero MQ for LabView) is the top-level function of the package.
 User functions must be supplied as a NamedTuple or Dict. Start ZMQ socket, listen to
 requests, parse them, execute the requested user function, send response. Repeat.
-
+# Arguments
+- `initOK`: set it to `true` to skip check of the user script
 # Examples
 ```julia-repl
 
 julia> server_0mq4lv((;foo=foo, bar, baz))
 ```
 """
-function server_0mq4lv(fns=(;))
+function server_0mq4lv(fns=(;); initOK=false)
 
     context = Context()
     socket = Socket(context, REP)
     ZMQ.bind(socket, "tcp://*:5555")
-    initOK = false
     version = string(PkgVersion.Version(LabView0mqJl))
     if isempty(fns)
         fnlist = "built-in test functions only"
@@ -31,24 +31,20 @@ function server_0mq4lv(fns=(;))
     global scriptexists
     global scriptOK
 
-    if isempty(fns)
-        initOK = true # package-internal functions only
-    else
-        try
-            # in case the server started with a user script
-            # from command line via LabVIEW
-            # otherwise if "using LabView0mqJl" manually
-            # init these two global variables manually, too
-            initOK = scriptexists && scriptOK
-        catch err
-            if isa(err, UndefVarError)
-                # this file was probably executed as standalone for development purposes
-                # you know what you do, so OK
-                initOK = true
+    if ! initOK
+        if isempty(fns)
+            initOK = true # package-internal functions only
+        else
+            try
+                # in case the server started with a user script
+                # from command line via LabVIEW
+                # otherwise if "using LabView0mqJl" manually
+                # init these two global variables manually, too
+                initOK = scriptexists && scriptOK
+            catch err
             end
         end
     end
-
 
     if initOK
         println("starting ZMQ server, Julia for LabVIEW: LabView0mqJl v=$version")
@@ -56,6 +52,7 @@ function server_0mq4lv(fns=(;))
         println(fnlist)
     else
         println("server initialisation failed")
+        @show scriptexists scriptOK
     end
 
     try
