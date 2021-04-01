57b3fd55552a8b6535be051c4e8ff9f423422bb9
Eben60 <8xe8n-cm@online.de>
2021-04-01 21:19:04 +0200

    minor changes

--- src/LabVIEW/jl-scripts/start-from-mac.jl
+++ src/LabVIEW/jl-scripts/start-from-mac.jl
@@ -1,7 +1,7 @@
 using LabView0mqJl
 
 try
-    include(get_script_path(raw"~/Desktop/Ruby-Python/Julia/LabView0mqJl.jl/src/LabVIEW/jl-scripts/09_combined_example.jl"))
+    include(get_script_path(raw"/users/eben60/Desktop/Julia/LabView0mqJl.jl/src/LabVIEW/jl-scripts/09_combined_example.jl"))
     LabView0mqJl.setglobals(isOK=true)
 catch excep
     LabView0mqJl.setglobals(isOK=false, excpn=(;excep, stack_trace=backtrace()))
