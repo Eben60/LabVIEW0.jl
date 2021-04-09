d7cb046863a89f39358aa42a16f56e887f464c52
Eben60 <8xe8n-cm@online.de>
2021-04-09 11:18:52 +0200

    transform_img

--- src/LabVIEW/jl-scripts/08-transform_img.jl
+++ src/LabVIEW/jl-scripts/08-transform_img.jl
@@ -1,3 +1,5 @@
+using Images # TODO do we need it?
+
 function transform_img(; img, flip_lr=false, flip_ud=false, negative=false)
     if flip_lr
         img = reverse(img, dims=2)
@@ -8,14 +10,10 @@ function transform_img(; img, flip_lr=false, flip_ud=false, negative=false)
     end
 
     if negative
-        img = map(x->typeof(x)(1-x.r,1-x.g,1-x.b), img)
+        img = map(x->typeof(x)(1-x.r,1-x.g,1-x.b), img) # works probably for RGB images only
     end
 
     return (; bigarrs = (;img, ))
 end
 
 fns = (; transform_img)
-
-# invpix(x) = typeof(x)(1-x.r,1-x.g,1-x.b)
-
-# map(x->typeof(x)(1-x.r,1-x.g,1-x.b)
