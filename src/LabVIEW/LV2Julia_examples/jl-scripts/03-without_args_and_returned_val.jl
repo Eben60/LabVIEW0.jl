3fbe458a50428aa8ac25308f1580dae22bc704c6
Eben60 <61665180+Eben60@users.noreply.github.com>
2022-01-20 22:52:56 +0100

    Update 03-without_args_and_returned_val.jl
    comment added

--- src/LabVIEW/LV2Julia_examples/jl-scripts/03-without_args_and_returned_val.jl
+++ src/LabVIEW/LV2Julia_examples/jl-scripts/03-without_args_and_returned_val.jl
@@ -1,4 +1,4 @@
-function hello_world_and_no_arguments(;)
+function hello_world_and_no_arguments(;) # Attention! The function must accept keyword arguments, therefore semicolon. Here just a zero number of kwargs
     println("Hello World!")
     return (;) # Attention! If no values are to be returned, then return an empty named tuple.
 end
